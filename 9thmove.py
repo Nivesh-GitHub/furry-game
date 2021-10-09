18ca661689fdf484c6326f6705f18a97c9cd1df8
Nivesh-GitHub <89345698+Nivesh-GitHub@users.noreply.github.com>
2021-10-09 11:27:48 +0530

    Create 9thmove.py

---
+++ 9thmove.py
@@ -0,0 +1,18 @@
+board = chess.Board()
+board.push_san("e4")
+board.push_san("e5")
+board.push_san("d4")
+board.push_san("Bb4+")
+board.push_san("c3")
+board.push_san("Ba5")
+board.push_san("b4")
+board.push_san("Bb6")
+board.push_san("a4")
+board.push_san("Nf6")
+board.push_san("dxe5")
+board.push_san("a6")
+board.push_san("Be3")
+
+
+
+board
